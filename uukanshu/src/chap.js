97fb0b43befaa7ee086ee4d031b8efc4015a130e
Darkrai9x <longvu.uet@gmail.com>
2021-03-27 18:23:55 +0700

    Update uukanshu

--- uukanshu/src/chap.js
+++ uukanshu/src/chap.js
@@ -1,7 +1,15 @@
 function execute(url) {
-    var doc = Http.get(url).html();
-    doc.select(".ad_content").remove();
-    var htm = doc.select("#contentbox").html();
+    var htm = "";
+    if (url.indexOf("sj.uukanshu.com") !== -1) {
+        var doc = Http.get(url).html();
+        doc.select(".ad_content").remove();
+        doc.select("div.box").remove();
+        htm = doc.select("#bookContent").html();
+    } else {
+        var doc = Http.get(url).html("gb2312");
+        doc.select(".ad_content").remove();
+        htm = doc.select("#contentbox").html();
+    }
     htm = htm.replace(/[UＵ][UＵ]\s*看书\s*[wｗ][wｗ][wｗ][\.．][uｕ][uｕ][kｋ][aａ][nｎ][sｓ][hｈ][uｕ][\.．][cｃ][oｏ][mｍ]/gi, "");
     htm = htm.replace(/\&nbsp;/g, "");
     return Response.success(htm);
--- uukanshu/src/detail.js
+++ uukanshu/src/detail.js
@@ -1,15 +1,31 @@
 function execute(url) {
-    const doc = Http.get(url).html();
-    var coverImg = doc.select(".bookImg img").first().attr("src");
-    if (coverImg.startsWith("//")) {
-        coverImg = "https:" + coverImg;
+    if (url.indexOf("sj.uukanshu.com") !== -1) {
+        const info = Http.get(url).html().select(".book-info");
+        var coverImg = info.select(".pic img").first().attr("src");
+        if (coverImg.startsWith("//")) {
+            coverImg = "https:" + coverImg;
+        }
+        return Response.success({
+            name: info.select("h1.bookname").text(),
+            cover: coverImg,
+            author: info.select("dd").first().text().replace("作者：", ""),
+            description: info.select(".desc").text(),
+            detail: info.select("dd").html(),
+            host: "https://sj.uukanshu.com"
+        });
+    } else {
+        const doc = Http.get(url).html();
+        var coverImg = doc.select(".bookImg img").first().attr("src");
+        if (coverImg.startsWith("//")) {
+            coverImg = "https:" + coverImg;
+        }
+        return Response.success({
+            name: doc.select(".jieshao_content h1").text().replace("最新章节", ""),
+            cover: coverImg,
+            author: doc.select(".jieshao_content h2 a").first().text(),
+            description: doc.select(".jieshao_content h3").text(),
+            detail: doc.select(".jieshao_content h2 a").html(),
+            host: "https://www.uukanshu.com"
+        });
     }
-    return Response.success({
-        name: doc.select(".jieshao_content h1").text().replace("最新章节", ""),
-        cover: coverImg,
-        author: doc.select(".jieshao_content h2 a").first().text(),
-        description: doc.select(".jieshao_content h3").text(),
-        detail: doc.select(".jieshao_content h2 a").html(),
-        host: "https://www.uukanshu.com"
-    });
 }
