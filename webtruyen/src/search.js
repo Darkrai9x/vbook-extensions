737e55ac5b01bc1fee9491d05aa40b392dcaa784
Darkrai9x <longvu.uet@gmail.com>
2021-06-29 21:25:50 +0700

    Update dtruyen

--- webtruyen/src/search.js
+++ webtruyen/src/search.js
@@ -2,17 +2,18 @@ function execute(key, page) {
     if (!page) page = '1';
     var doc = Http.get("https://dtruyen.com/searching/" + key.replace(" ", "-") + "/lastupdate/all/all/" + page).html();
 
-    var next = doc.select(".pagination").select("li:has(.active) + li").select("a").text();
+     var next = doc.select(".pagination").select("li.active + li").text();
 
-    var el = doc.select(".list-content .list-row-img");
-    var data = [];
+    const el = doc.select(".list-stories .story-list")
+
+    const data = [];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
         data.push({
-            name: e.select(".row-info a").first().text(),
-            link: e.select(".row-info a").first().attr("href"),
-            cover: e.select(".row-image img").first().attr("src"),
-            description: e.select(".row-author").text(),
+            name: e.select(".info h3").first().text(),
+            link: e.select(".info h3 a").first().attr("href"),
+            cover: e.select(".thumb img").first().attr("data-layzr"),
+            description: e.select(".last-chapter").first().text(),
             host: "https://dtruyen.com"
         })
     }
