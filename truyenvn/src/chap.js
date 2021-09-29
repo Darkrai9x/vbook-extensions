29b6e6c21a0ce03b30861b6bae17eae7aa5eeaa5
Darkrai9x <longvu.uet@gmail.com>
2021-09-29 20:58:46 +0700

    Update TruyenVN

--- truyenvn/src/chap.js
+++ truyenvn/src/chap.js
@@ -1,15 +1,15 @@
 function execute(url) {
     url = url.replace("truyenvn.com", "truyenvn.tv");
     var doc = Http.get(url).html();
-    var postId = doc.select("form input[name=p]").first().attr("value")
-    var json = Http.post("https://truyenvn.tv/wp-admin/admin-ajax.php").params({
-        action:  "z_do_ajax",
-        _action:  "load_imgs_for_chapter",
-        p: postId
-    }).string();
-    var data = JSON.parse(json);
-    var imgs  = data.mes.map(item => {
-        return item.url.replace("sv5.webtruyen.info","sv5.ghienmanga.net");
-    });
+    var el = doc.select(".content-text img");
+    var imgs = [];
+    for (var i = 0; i < el.size(); i++) {
+        var e = el.get(i);
+        var url = e.attr("src");
+        if (!url.endsWith('truyenvn-tv-banner.jpg')
+            && !url.endsWith('credit-truyenvn-tv.jpg')) {
+            imgs.push(url);
+        }
+    }
     return Response.success(imgs);
 }
