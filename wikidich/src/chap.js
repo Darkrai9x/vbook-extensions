7fb42e561a15f5111c3d889bdc0b253e53533470
Darkrai9x <longvu.uet@gmail.com>
2021-11-28 00:02:46 +0700

    Update wiki

--- wikidich/src/chap.js
+++ wikidich/src/chap.js
@@ -1,100 +1,43 @@
 function execute(url) {
     url = url.replace("wikidich.com", "wikidth.com");
-    let response = fetch(url);
-    if (response.ok) {
-        let doc = response.html();
-        let parts = doc.select(".chapter-part");
-        if (parts.length === 0) {
-            return Response.success(doc.select("div#bookContentBody").html());
-        } else {
-            let html = doc.html();
-            let content = "";
-            for (let i = 0; i < parts.size() / 2; i++) {
-                let part = parts.get(i);
-                let partContent = loadChapterPart(html, part.attr("data-id"), part.attr("data-type"), part.attr("data-pn"));
-                if (partContent) {
-                    content += partContent + "<br/>";
-                }
+    var doc = Http.get(url).html();
+    var parts = doc.select(".chapter-part");
+    if (parts.size() == 0) {
+        return Response.success(doc.select("div#bookContentBody").html());
+    } else {
+        var html = doc.html();
+        var content = "";
+        for (var i = 0; i < parts.size() / 2; i++) {
+            var part = parts.get(i);
+            var partContent = loadChapterPart(html, part.attr("data-id"), part.attr("data-type"), part.attr("data-pn"));
+            if (partContent) {
+                content += partContent + "<br/>";
             }
-            return Response.success(content);
         }
+        return Response.success(content);
     }
-    return null;
 }
 
 
 function loadChapterPart(html, id, type, pn) {
 
-    let signKey = html.match(/signKey\s*=\s*"(.*?)";/);
+    var signKey = html.match(/signKey\s*=\s*"(.*?)";/);
     if (signKey) signKey = signKey[1];
 
-    let fuzzySign = html.match(/function fuzzySign[\s\S]*?}/);
+    var fuzzySign = html.match(/function fuzzySign[\s\S]*?}/);
     if (fuzzySign) fuzzySign = fuzzySign[0];
 
-    let result = Script.execute(fuzzySign, "fuzzySign", signKey + type + pn + false);
-    let signFunc = function a(W) {
-        function V(d, c) {
-            return d >>> c | d << 32 - c
-        }
-        for (var U, T, S = Math.pow, R = S(2, 32), Q = "length", P = "", O = [], N = 8 * W[Q], M = a.h = a.h || [], L = a.k = a.k || [], K = L[Q], J = {}, I = 2; 64 > K; I++) {
-            if (!J[I]) {
-                for (U = 0; 313 > U; U += I) {
-                    J[U] = I
-                }
-                M[K] = S(I, 0.5) * R | 0,
-                    L[K++] = S(I, 1 / 3) * R | 0
-            }
-        }
-        for (W += "\x80"; W[Q] % 64 - 56;) {
-            W += "\x00"
-        }
-        for (U = 0; U < W[Q]; U++) {
-            if (T = W.charCodeAt(U),
-                T >> 8) {
-                return
-            }
-            O[U >> 2] |= T << (3 - U) % 4 * 8
-        }
-        for (O[O[Q]] = N / R | 0,
-            O[O[Q]] = N,
-            T = 0; T < O[Q];) {
-            var H = O.slice(T, T += 16)
-                , G = M;
-            for (M = M.slice(0, 8),
-                U = 0; 64 > U; U++) {
-                var F = H[U - 15]
-                    , E = H[U - 2]
-                    , D = M[0]
-                    , C = M[4]
-                    , B = M[7] + (V(C, 6) ^ V(C, 11) ^ V(C, 25)) + (C & M[5] ^ ~C & M[6]) + L[U] + (H[U] = 16 > U ? H[U] : H[U - 16] + (V(F, 7) ^ V(F, 18) ^ F >>> 3) + H[U - 7] + (V(E, 17) ^ V(E, 19) ^ E >>> 10) | 0)
-                    , A = (V(D, 2) ^ V(D, 13) ^ V(D, 22)) + (D & M[1] ^ D & M[2] ^ M[1] & M[2]);
-                M = [B + A | 0].concat(M),
-                    M[4] = M[4] + B | 0
-            }
-            for (U = 0; 8 > U; U++) {
-                M[U] = M[U] + G[U] | 0
-            }
-        }
-        for (U = 0; 8 > U; U++) {
-            for (T = 3; T + 1; T--) {
-                var z = M[U] >> 8 * T & 255;
-                P += (16 > z ? 0 : "") + z.toString(16)
-            }
-        }
-        return P
-    };
-    let sign = signFunc(result);
-    let content = fetch("https://wikidth.com/chapters/part", {
-        method: "POST",
-        body: {
-            id: id,
-            type: type,
-            pn: pn,
-            en: false,
-            signKey: signKey,
-            sign: sign
-        }
-    }).json();
+    var result = Script.execute(fuzzySign, "fuzzySign", signKey + type + pn + false);
+    var signFunc = "function signFunc(r){function o(r,o){return r>>>o|r<<32-o}for(var f,n,t=Math.pow,c=t(2,32),i=\"length\",a=\"\",e=[],u=8*r[i],v=[],g=[],h=g[i],l={},s=2;64>h;s++)if(!l[s]){for(f=0;313>f;f+=s)l[f]=s;v[h]=t(s,.5)*c|0,g[h++]=t(s,1/3)*c|0}for(r+=\"\";r[i]%64-56;)r+=\"\\0\";for(f=0;f<r[i];f++){if((n=r.charCodeAt(f))>>8)return;e[f>>2]|=n<<(3-f)%4*8}for(e[e[i]]=u/c|0,e[e[i]]=u,n=0;n<e[i];){var d=e.slice(n,n+=16),p=v;for(v=v.slice(0,8),f=0;64>f;f++){var w=d[f-15],A=d[f-2],C=v[0],F=v[4],M=v[7]+(o(F,6)^o(F,11)^o(F,25))+(F&v[5]^~F&v[6])+g[f]+(d[f]=16>f?d[f]:d[f-16]+(o(w,7)^o(w,18)^w>>>3)+d[f-7]+(o(A,17)^o(A,19)^A>>>10)|0);(v=[M+((o(C,2)^o(C,13)^o(C,22))+(C&v[1]^C&v[2]^v[1]&v[2]))|0].concat(v))[4]=v[4]+M|0}for(f=0;8>f;f++)v[f]=v[f]+p[f]|0}for(f=0;8>f;f++)for(n=3;n+1;n--){var S=v[f]>>8*n&255;a+=(16>S?0:\"\")+S.toString(16)}return a}";
+    var sign = Script.execute(signFunc, "signFunc", result);
+    var content = Http.post("https://wikidth.com/chapters/part").params({
+        id: id,
+        type: type,
+        pn: pn,
+        en: false,
+        signKey: signKey,
+        sign: sign
+    }).string()
 
-    return content.data.content
+    return JSON.parse(content).data.content
 }
