c6bf50741face8d2aca06cddbafd2534cf8aa88e
Darkrai9x <longvu.uet@gmail.com>
2021-12-04 10:01:17 +0700

    Update umetruyen

---
+++ umetruyen/src/cat.js
@@ -0,0 +1,28 @@
+function execute(url, page) {
+    if (!page) {
+        page = "1";
+    }
+
+    let response = fetch(url + "?page=" + page);
+
+    if (response.ok) {
+        let doc = response.html();
+        let next = doc.select(".pager").select("li.active + li").text();
+
+        let novelList = [];
+        doc.select(".manga-content .page-item-detail").forEach(e => {
+            let cover = e.select("img").first().attr("data-src");
+            if (!cover) cover = e.select("img").first().attr("src");
+            novelList.push({
+                name: e.select("h3 a").first().text(),
+                link: e.select("h3 a").first().attr("href"),
+                cover: cover,
+                description: e.select(".chapter").first().text(),
+                host: "https://umetruyen.net"
+            });
+        })
+        return Response.success(novelList, next)
+    }
+
+    return null;
+}
--- umetruyen/src/home.js
+++ umetruyen/src/home.js
@@ -1,5 +1,6 @@
 function execute() {
     return Response.success([
+        {title: "Mới cập nhật", input: "https://umetruyen.net", script: "cat.js"},
         {title: "Manhwa", input: "https://umetruyen.net/genre/manhwa", script: "gen.js"},
         {title: "Manga", input: "https://umetruyen.net/genre/manga", script: "gen.js"},
         {title: "Manhua", input: "https://umetruyen.net/genre/manhua", script: "gen.js"},
