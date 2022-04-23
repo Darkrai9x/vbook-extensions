4ee839471b74210a1bf5d78f6823d20a4c063d35
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-04-23 20:41:29 +0700

    fix chivi

--- chivi/src/toc.js
+++ chivi/src/toc.js
@@ -1,13 +1,12 @@
 function execute(url) {
     let storyUrl = url.split(" ");
     let response = fetch(storyUrl[1]);
-
     if (response.ok) {
         let data = response.json();
         var sname = data.props.chseed.sname;
         let chapList = data.props.chpage.chaps.map(e => ({
             "name": e.title,
-            "url": storyUrl[0] + "/chaps/" + sname + "/" + e.uslug + "-" + e.chidx,
+            "url": storyUrl[0] + "/-" + sname + "/" + e.chidx + "-" + e.uslug,
             "host": "https://chivi.app"
         }));
         return Response.success(chapList);
