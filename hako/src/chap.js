af966e42229e6414e9a5a6b099137507ef6e964d
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-05-01 19:46:10 +0700

    fix thẻ p sau khi xuất epub Hako

--- hako/src/chap.js
+++ hako/src/chap.js
@@ -3,7 +3,10 @@ function execute(url) {
     if (response.ok) {
         let doc = response.html();
         doc.select(".note-reg").remove();
-        return Response.success(doc.select("div#chapter-content").html());
+        htm = doc.select("div#chapter-content");
+        htm.select("p.none").remove();
+        htm = htm.html().replace(/<p id=\"\d+\">/g, "<p>").replace(/\[note\d+]/g, "")
+        return Response.success(htm);
     }
     return null;
 }
