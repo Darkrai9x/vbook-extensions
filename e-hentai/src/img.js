6682ef488cbd8cf307876c42915114afcf441ed9
Darkrai9x <longvu.uet@gmail.com>
2022-05-07 17:48:25 +0700

    Update E-Hentai

--- e-hentai/src/img.js
+++ e-hentai/src/img.js
@@ -1,7 +1,15 @@
 function execute(url) {
-    let response = fetch(url);
+    var response = fetch(url);
     if (response.ok) {
+        if (url.indexOf("nl=") === -1) {
+            let txt = response.text();
+            var nl = /return nl\('(.*?)'\)/.exec(txt);
+            if (nl) nl = nl[1];
+            else nl = "";
+            response = fetch(url + "?nl=" + nl);
+        }
         let doc = response.html();
+       
         return doc.select("#img").attr("src");
     }
     return null;
