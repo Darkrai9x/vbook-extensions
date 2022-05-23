d741bf6b46452ece1920ddc32d0abdc2c017f48c
Lê Thanh Lâm <34809211+Moleys@users.noreply.github.com>
2022-05-23 20:12:32 +0700

    xóa ảnh banner ở cuối chap truyện Hako

--- hako/src/chap.js
+++ hako/src/chap.js
@@ -5,7 +5,10 @@ function execute(url) {
         doc.select(".note-reg").remove();
         htm = doc.select("div#chapter-content");
         htm.select("p.none").remove();
-        htm = htm.html().replace(/<p id=\"\d+\">/g, "<p>").replace(/\[note\d+]/g, "")
+        htm.select('img[src*="/images/banners/"]').remove();
+        htm.select("p:contains(Tham gia Discord Hako tại)").remove();
+        htm = htm.html().replace(/<p id=\"\d+\">/g, "<p>").replace(/\[note\d+]/g, "");
+        htm = htm.replace(/\&nbsp;/g, "");
         return Response.success(htm);
     }
     return null;
