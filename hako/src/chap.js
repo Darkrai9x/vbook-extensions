3e8089444bc3d85c272d28278132e2c8a021e074
Vũ Thanh Tùng <at160758@actvn.edu.vn>
2022-05-25 23:28:40 +0700

    Update chap.js

--- hako/src/chap.js
+++ hako/src/chap.js
@@ -2,13 +2,18 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        doc.select(".note-reg").remove();
-        htm = doc.select("div#chapter-content");
+        htm = doc.select("div#chapter-content")
         htm.select("p.none").remove();
         htm.select('img[src*="/images/banners/"]').remove();
         htm.select("p:contains(Tham gia Discord Hako tại)").remove();
-        htm = htm.html().replace(/<p id=\"\d+\">/g, "<p>").replace(/\[note\d+]/g, "");
+        htm = htm.html().replace(/<p id=\"\d+\">/g, "<p>");
         htm = htm.replace(/\&nbsp;/g, "");
+        let collection = doc.select("div.note-reg a.none-print")
+        collection.forEach((element) => {
+            let note = element.attr("href").replace("#anchor-","")
+            let note_content  = doc.select("#" + note +" span.note-content").text()
+            htm = htm.replace("["+note+"]"," <i>(📝 "+note_content+")<\/i>")
+        });        
         return Response.success(htm);
     }
     return null;
