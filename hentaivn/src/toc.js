710436ec03261a149986cb3a4cefab5ad1a579ff
fusornat <fusornat@gmail.com>
2022-12-06 00:45:44 +0700

    update 2tenvn

--- hentaivn/src/toc.js
+++ hentaivn/src/toc.js
@@ -3,9 +3,10 @@ function execute(url) {
     url = url.replace("hentaivn.tv", "hentaivn.moe");
     url = url.replace("hentaivn.moe", "hentaivn.fun");
     url = url.replace("hentaivn.fun", "hentaivn.la");
+    url = url.replace("hentaivn.la", "hentaivn.in");
     let response = fetch(url, {
         headers: {
-            "referer": "https://hentaivn.la"
+            "referer": "https://hentaivn.in"
         }
     });
     if (response.ok) {
@@ -15,27 +16,27 @@ function execute(url) {
         if (isMobile) {
             let getUrl = doc.html().match(/(list-showchapter-mobile.php.*?)\"/)[1];
             
-            doc = fetch("https://hentaivn.la/" + getUrl).html();
+            doc = fetch("https://hentaivn.in/" + getUrl).html();
             let el = doc.select(".episodes a")
             for (let i = el.size() - 1; i >= 0; i--) {
                 let e = el.get(i);
                 data.push({
                     name: e.text(),
                     url: e.attr("href"),
-                    host: "https://hentaivn.la"
+                    host: "https://hentaivn.in"
                 })
             }
         } else {
            let getUrl = doc.html().match(/(list-showchapter.php.*?)\"/)[1];
             
-            doc = fetch("https://hentaivn.la/" + getUrl).html();
+            doc = fetch("https://hentaivn.in/" + getUrl).html();
             let el = doc.select("a")
             for (let i = el.size() - 1; i >= 0; i--) {
                 let e = el.get(i);
                 data.push({
                     name: e.text(),
                     url: e.attr("href"),
-                    host: "https://hentaivn.la"
+                    host: "https://hentaivn.in"
                 })
             }
         }
