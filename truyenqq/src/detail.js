c7f1a115834d5fd9fae12c9334f7a56c1f1bdd2c
Alesha Nguyen <87790414+seyah24@users.noreply.github.com>
2022-12-14 10:39:50 +0700

    fix url

--- truyenqq/src/detail.js
+++ truyenqq/src/detail.js
@@ -4,7 +4,7 @@ function execute(url) {
     url = url.replace("truyenqq.com", "truyenqq.net");
     url = url.replace("truyenqq.net", "truyenqqtop.com");
     url = url.replace("truyenqqtop.com", "truyenqqvip.com");
-    url = url.replace("truyenqqvip.com", "truyenqqvip.com");
+    url = url.replace("truyenqqvip.com", "truyenqqpro.com");
     var doc = bypass(url, Http.get(url).html());
     if (doc) {
         var cover = doc.select(".book_avatar img").first().attr("src");
