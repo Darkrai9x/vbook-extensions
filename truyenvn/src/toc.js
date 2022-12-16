197867c41377bb4646a865734ed7244e3a996f0c
Darkrai9x <longvu.uet@gmail.com>
2022-12-16 21:09:10 +0700

    Update TruyenVN

--- truyenvn/src/toc.js
+++ truyenvn/src/toc.js
@@ -1,5 +1,5 @@
 function execute(url) {
-    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, "https://truyenvnhot.com");
+    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, "https://truyenvnhot.net");
 
     let response = fetch(url);
     if (response.ok) {
