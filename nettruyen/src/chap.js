a1e8f41a6e9e51b7d4bd4f4ad75253e636fdb3a2
Darkrai9x <longvu.uet@gmail.com>
2023-03-30 19:24:41 +0700

    Fix NetTruyen

--- nettruyen/src/chap.js
+++ nettruyen/src/chap.js
@@ -8,14 +8,26 @@ function execute(url) {
         let data = [];
         doc.select(".page-chapter img").forEach(e => {
             let img = e.attr("data-original");
+            let dataCdn = e.attr('data-cdn')
             if (!img) {
                 img = e.attr("src");
+            }
+            if (dataCdn) {
+                if (dataCdn.startsWith("//")) {
+                    dataCdn = "https:" + dataCdn;
+                }
             }
             if (img) {
                 if (img.startsWith("//")) {
                     img = "http:" + img;
                 }
-                data.push(img);
+                data.push({
+                    link: img,
+                    fallback: [
+                        dataCdn,
+                        'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&gadget=a&no_expand=1&resize_h=0&rewriteMime=image/*&url=' + encodeURIComponent(img)
+                    ]
+                });
             }
         });
         return Response.success(data);
