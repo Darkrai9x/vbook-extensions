ea48dae42460a0d8288892f0656a7c7dfe4dd52b
Darkrai9x <longvu.uet@gmail.com>
2023-04-28 19:46:11 +0700

    Fix BNS

--- bachngocsach/src/home.js
+++ bachngocsach/src/home.js
@@ -1,9 +1,11 @@
 load('host.js');
 function execute() {
     return Response.success([
-        {title: "Truyện Dịch", input: HOST + "/reader/recent-bns", script: "tab.js"},
-        {title: "Truyện Convert", input: HOST + "https://truyenbns.com/reader/recent-cv", script: "tab.js"},
+        {title: "Đề cử", input: HOST + "/reader/recent-promote", script: "tab.js"},
+        {title: "Dịch", input: HOST + "/reader/recent-bns", script: "tab.js"},
+        {title: "Convert", input: HOST + "/reader/recent-cv", script: "tab.js"},
         {title: "Sáng Tác", input: HOST + "/reader/recent-sangtac", script: "tab.js"},
-        {title: "Sưu tầm", input: HOST + "/reader/recent-st", script: "tab.js"}
+        {title: "Sưu tầm", input: HOST + "/reader/recent-st", script: "tab.js"},
+        {title: "Hoàn thành", input: HOST + "/reader/recent-hoanthanh", script: "tab.js"}
     ]);
 }
