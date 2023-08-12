dea215e7b50d3775c9cd02226928a273bd513f76
Darkrai9x <longvu.uet@gmail.com>
2023-08-12 20:09:22 +0700

    Update HentaiVN

--- hentaivn/src/chap.js
+++ hentaivn/src/chap.js
@@ -1,4 +1,5 @@
 load('config.js');
+
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
     let response = fetch(url, {
@@ -9,22 +10,11 @@ function execute(url) {
 
     if (response.ok) {
         let doc = response.html();
-
-        let newUrl = BASE_URL +"/" + /(list-loadchapter.php.*?)"/.exec(doc.html())[1];
-        response = fetch(newUrl, {
-           method:"GET",
-            headers: {
-               "Referer": url
-            }
+        let data = [];
+        doc.select("#image img").forEach(e => {
+            data.push(e.attr("data-src"));
         });
-        if (response.ok) {
-            doc = response.html();
-            let data = [];
-            doc.select("img").forEach(e => {
-                data.push(e.attr("src"));
-            });
-            return Response.success(data);
-        }
+        return Response.success(data);
     }
 
     return null;
