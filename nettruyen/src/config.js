10401e9bced2206a9433c14a2c0d2df96be1cf2d
Darkrai9x <longvu.uet@gmail.com>
2024-03-06 22:51:14 +0700

    Update Nettruyen

--- nettruyen/src/config.js
+++ nettruyen/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = 'https://www.nettruyenbb.com';
+let BASE_URL = 'https://www.nettruyenee.com';
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
--- truyenvn/src/config.js
+++ truyenvn/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://truyenvn.fun";
+let BASE_URL = "https://truyenvn.io";
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
