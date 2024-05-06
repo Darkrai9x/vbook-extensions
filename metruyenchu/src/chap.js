d1ece6df3db64d9b135d90b1e6ab645281c7c1d6
Darkrai9x <longvu.uet@gmail.com>
2024-05-06 18:48:58 +0700

    Update MTC

--- metruyenchu/src/chap.js
+++ metruyenchu/src/chap.js
@@ -6,7 +6,7 @@ function execute(url) {
 
     if (response.ok) {
         let doc = response.html();
-        let content = doc.select("[data-x-bind=ChapterContent]").first();
+        let content = doc.select("[data-x-bind=ChapterContent]").first().html();
         return Response.success(content);
     }
     return null;
