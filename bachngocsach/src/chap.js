534a2c020e133aa0571c7955ec1ab73eaad5739a
Darkrai9x <longvu.uet@gmail.com>
2024-11-23 20:20:31 +0700

    Update BNS

--- bachngocsach/src/chap.js
+++ bachngocsach/src/chap.js
@@ -1,9 +1,25 @@
 load('config.js');
+
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
 
     let response = fetch(url);
-    if (response.ok)
+    if (response.ok) {
+        let doc = response.html();
+        let encryptedContent = doc.select("#encrypted-content").text();
+        if (encryptedContent) {
+            response = fetch(BASE_URL + "/reader/api/decrypt-content.php", {
+                method: "POST",
+                body: JSON.stringify({
+                    "encryptedData": encryptedContent
+                })
+            });
+
+            let json = response.json();
+
+            return Response.success(json.content);
+        }
         return Response.success(response.html().select("#noi-dung").html());
+    }
     return null;
 }
