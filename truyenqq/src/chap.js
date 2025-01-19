bee1dba945354e56a5e176e4af8d67a87bcdef4c
Darkrai9x <longvu.uet@gmail.com>
2025-01-19 12:51:32 +0700

    UpdateTruyen

--- truyenqq/src/chap.js
+++ truyenqq/src/chap.js
@@ -9,11 +9,27 @@ function execute(url) {
         let data = [];
         for (let i = 0; i < imgs.size(); i++) {
             let e = imgs.get(i);
-            let dataOriginal = e.attr("data-original")
-            let dataCdn = e.attr("data-cdn");
+            let link = e.attr("data-original")
+            if (link !== undefined) {
+                if (link.indexOf("mangaqq.net") > -1 || link.indexOf("cdnqq.xyz") > -1) {
+                    link = link.replace("mangaqq.net", "i200.truyenvua.com");
+                    link = link.replace("cdnqq.xyz", "i200.truyenvua.com");
+                } else if (link.indexOf("mangaqq.com") > -1) {
+                    link = link.replace("mangaqq.com", "i216.truyenvua.com");
+                } else if (link.indexOf("trangshop.net") > -1 || link.indexOf("photruyen.com") > -1 || link.indexOf("tintruyen.com") > -1) {
+                    link = link.replace("photruyen.com", "i109.truyenvua.com");
+                    link = link.replace("tintruyen.com", "i109.truyenvua.com");
+                    link = link.replace("trangshop.net", "i109.truyenvua.com");
+                } else if (link.indexOf("tintruyen.net") > -1) {
+                    link = link.replace("//tintruyen.net", "//i138.truyenvua.com");
+                    link = link.replace("//i125.tintruyen.net", "//i125.truyenvua.com");
+                } else if (link.indexOf("qqtaku.com") > -1) {
+                    link = link.replace("qqtaku.com", "i125.truyenvua.com");
+                }
+            }
             data.push({
                 link: e.attr("src"),
-                fallback: [dataOriginal, dataCdn]
+                fallback: [link]
             });
         }
         return Response.success(data);
