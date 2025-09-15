c276d545de4c456c6f4f9258fd4efa56e22ce5e8
Darkrai9x <longvu.uet@gmail.com>
2025-09-15 21:46:41 +0700

    Update Banlong

--- banlong/src/page.js
+++ banlong/src/page.js
@@ -1,19 +1,15 @@
 load("config.js");
 
 function execute(url) {
-    let response = fetch(url);
-    if (response.ok) {
-        let doc = response.html();
-        let storyId = doc.select("input[name=story]").first().attr("value");
-        let json = fetch(BASE_URL.replace("https://", "https://api.") + "/v1/story/" + storyId + "/chapter_list?page=1&new=0").json();
-        let lastPage = json.total_page;
-        let pages = [];
-        for (let i = 1; i <= lastPage; i++) {
-            pages.push("v1/story/" + storyId + "/chapter_list?page=" + i + "&new=0");
-        }
+    let storyId = url.split('/').pop();
 
-        return Response.success(pages);
+
+    let json = fetch(BASE_URL.replace("https://", "https://api.") + "/v1/story/" + storyId + "/chapter_list?page=1&new=0").json();
+    let lastPage = json.total_page;
+    let pages = [];
+    for (let i = 1; i <= lastPage; i++) {
+        pages.push("v1/story/" + storyId + "/chapter_list?page=" + i + "&new=0");
     }
 
-    return null;
+    return Response.success(pages);
 }
