96ae29713465f0a1201389880f034d2429e5142c
dat-bi <83175246+dat-bi@users.noreply.github.com>
2026-02-04 20:24:21 +0700

    fix nhentai

--- nhentai/src/chap.js
+++ nhentai/src/chap.js
@@ -1,16 +1,34 @@
 function execute(url) {
     let response = fetch(url);
     if (response.ok) {
-
         let doc = response.html();
-        let el = doc.select("div#thumbnail-container div.thumb-container a");
-        var mediaServer = /media_server\s*:\s*(\d+)/g.exec(doc.html());
-        if (mediaServer) mediaServer = mediaServer[1];
-        el.select("noscript").remove();
+        let htmlContent = doc.html();
+        
+        // 1. Lấy Media Server (ưu tiên từ script, nếu không có thì mặc định là 3 hoặc 5)
+        let mediaServerMatch = /media_server\s*:\s*(\d+)/.exec(htmlContent);
+        let mediaServer = mediaServerMatch ? mediaServerMatch[1] : "3"; 
+
+        // 2. Chọn chính xác các ảnh thumbnail
+        let el = doc.select("#thumbnail-container img.lazyload");
         let data = [];
+
         el.forEach(e => {
-            data.push(e.select("img").attr("data-src").replace(/t(\d+)?.nhentai.net/g, "i" + mediaServer + ".nhentai.net").replace(/(\d+)t/, "$1"));
+            let thumbSrc = e.attr("data-src");
+            if (thumbSrc) {
+                let match = /\/galleries\/(\d+)\/(\d+)t\.(webp|jpg|png)/.exec(thumbSrc);
+                
+                if (match) {
+                    let galleryId = match[1];
+                    let pageNum = match[2];
+                    let extension = match[3];
+                    
+                    let originalUrl = "https://i" + mediaServer + ".nhentai.net/galleries/" + galleryId + "/" + pageNum + "." + extension;
+                    
+                    data.push(originalUrl);
+                }
+            }
         });
+
         return Response.success(data);
     }
     return null;
--- nhentai/src/detail.js
+++ nhentai/src/detail.js
@@ -3,7 +3,7 @@ function execute(url) {
 
     return Response.success({
         name: doc.select(".title .pretty").first().text(),
-        cover: doc.select("#cover img").first().attr("data-src"),
+        cover:"https:"+  doc.select("#cover img").first().attr("data-src"),
         author: doc.select("a[href^=/artist/]").first().text(),
         description: doc.select("#tags").html(),
         detail: doc.select("#info").html(),
