1fa2805db4e830c1f6f87e1e4dac254bec1e856d
LongVD <longvu.uet@gmail.com>
2026-03-26 23:54:25 +0700

    Update truyenqq

--- truyenqq/src/home.js
+++ truyenqq/src/home.js
@@ -1,11 +1,12 @@
 function execute() {
     return Response.success([
-        {title: "Top Ngày", input: "/top-ngay.html", script: "gen.js"},
-        {title: "Top Tuần", input: "/top-tuan.html", script: "gen.js"},
-        {title: "Top Tháng", input: "/top-thang.html", script: "gen.js"},
-        {title: "Yêu Thích", input: "/truyen-yeu-thich.html", script: "gen.js"},
-        {title: "Mới Cập Nhật", input: "/truyen-moi-cap-nhat.html", script: "gen.js"},
-        {title: "Truyện Mới", input: "/truyen-tranh-moi.html", script: "gen.js"},
-        {title: "Truyện Full", input: "/truyen-hoan-thanh.html", script: "gen.js"}
+        {title: "Top Ngày", input: "/top-ngay", script: "gen.js"},
+        {title: "Top Tuần", input: "/top-tuan", script: "gen.js"},
+        {title: "Top Tháng", input: "/top-thang", script: "gen.js"},
+        {title: "Yêu Thích", input: "/truyen-yeu-thich", script: "gen.js"},
+        {title: "Mới Cập Nhật", input: "/truyen-moi-cap-nhat", script: "gen.js"},
+        {title: "Truyện Mới", input: "/truyen-tranh-moi", script: "gen.js"},
+        {title: "Truyện Full", input: "/truyen-hoan-thanh", script: "gen.js"},
+        {title: "Truyện Ngẫu Nhiên", input: "/truyen-ngau-nhien", script: "gen.js"}
     ]);
 }
