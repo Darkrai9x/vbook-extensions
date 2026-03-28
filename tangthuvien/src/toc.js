e7383dfd43f98b97d136fd7fb4be8c0e59124f27
LongVD <longvu.uet@gmail.com>
2026-03-28 22:00:12 +0700

    Remove TTV

--- tangthuvien/src/toc.js
+++ tangthuvien/src/toc.js
@@ -2,13 +2,13 @@ load("config.js");
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
     let response = fetch(url, {
-        headers: {"user-agent": UserAgent.chrome()},
+        headers: {"user-agent": UserAgent.android()},
     });
     if (response.ok) {
         let doc = response.html();
         let id = doc.select("input[name=story_id]").attr("value");
         response = fetch(BASE_URL + "/story/chapters?story_id=" + id, {
-            headers: {"user-agent": UserAgent.chrome()},
+            headers: {"user-agent": UserAgent.android()},
         });
         if (response.ok) {
             doc = response.html();
